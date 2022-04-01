5de35ff714c9e44de222f3dd26f6e57ab0216ffd
ahassan@azavea.com <adeel.hassan93@gmail.com>
2022-04-01 15:02:53 +0500

    add scene groups to DatasetConfig

--- rastervision_core/rastervision/core/data/dataset_config.py
+++ rastervision_core/rastervision/core/data/dataset_config.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import Dict, List, Set
 
-from rastervision.pipeline.config import (Config, register_config, ConfigError)
+from rastervision.pipeline.config import (Config, register_config, ConfigError,
+                                          Field)
 from rastervision.pipeline.utils import split_into_groups
 from rastervision.core.data.scene_config import SceneConfig
 from rastervision.core.data.class_config import ClassConfig
@@ -23,6 +24,12 @@ class DatasetConfig(Config):
     train_scenes: List[SceneConfig]
     validation_scenes: List[SceneConfig]
     test_scenes: List[SceneConfig] = []
+    scene_groups: Dict[str, Set[str]] = Field(
+        {},
+        description='Groupings of scenes. Should be a dict of the form: '
+        '{<group-name>: Set(scene_id_1, scene_id_2, ...)}. Four groups are '
+        'added by default: "all_scenes", "train_scenes", "validation_scenes", '
+        'and "test_scenes"')
 
     def update(self, pipeline=None):
         super().update()
@@ -36,6 +43,15 @@ class DatasetConfig(Config):
             for s in self.test_scenes:
                 s.update(pipeline=pipeline)
 
+        # add default scene groups
+        self.scene_groups['all_scenes'] = {s.id for s in self.all_scenes}
+        self.scene_groups['train_scenes'] = {s.id for s in self.train_scenes}
+        self.scene_groups['test_scenes'] = {s.id for s in self.test_scenes}
+        self.scene_groups['validation_scenes'] = {
+            s.id
+            for s in self.validation_scenes
+        }
+
     def validate_config(self):
         ids = [s.id for s in self.train_scenes]
         if len(set(ids)) != len(ids):
@@ -45,6 +61,14 @@ class DatasetConfig(Config):
         if len(set(ids)) != len(ids):
             raise ConfigError(
                 'All validation and test scene ids must be unique.')
+
+        all_ids = {s.id for s in self.all_scenes}
+        for group_name, group_ids in self.scene_groups.items():
+            for id in group_ids:
+                if id not in all_ids:
+                    raise ConfigError(
+                        f'ID "{id}" in scene group "{group_name}" does not '
+                        'match the ID of any scene in the dataset.')
 
     def get_split_config(self, split_ind, num_splits):
         new_cfg = self.copy()
