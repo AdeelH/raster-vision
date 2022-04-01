12acfc788c0132d97b8a424448cd7a6c8d5efee4
ahassan@azavea.com <adeel.hassan93@gmail.com>
2022-04-01 15:57:15 +0500

    make EvaluatorConfigs set output_uri based on scene group
    also remove vector_output_uri from SSEvaluatorConfig

--- rastervision_core/rastervision/core/evaluation/chip_classification_evaluator_config.py
+++ rastervision_core/rastervision/core/evaluation/chip_classification_evaluator_config.py
@@ -1,11 +1,19 @@
+from typing import TYPE_CHECKING, Iterable, Tuple
 from rastervision.pipeline.config import register_config
 from rastervision.core.evaluation.classification_evaluator_config import (
     ClassificationEvaluatorConfig)
 from rastervision.core.evaluation.chip_classification_evaluator import (
     ChipClassificationEvaluator)
 
+if TYPE_CHECKING:
+    from rastervision.core.data import ClassConfig
+
 
 @register_config('chip_classification_evaluator')
 class ChipClassificationEvaluatorConfig(ClassificationEvaluatorConfig):
-    def build(self, class_config):
-        return ChipClassificationEvaluator(class_config, self.output_uri)
+    def build(self, class_config: 'ClassConfig',
+              scene_group: Tuple[str, Iterable[str]]
+              ) -> ChipClassificationEvaluator:
+        group_name, _ = scene_group
+        output_uri = self.get_output_uri(group_name)
+        return ChipClassificationEvaluator(class_config, output_uri)
--- rastervision_core/rastervision/core/evaluation/object_detection_evaluator_config.py
+++ rastervision_core/rastervision/core/evaluation/object_detection_evaluator_config.py
@@ -1,11 +1,20 @@
+from typing import TYPE_CHECKING, Iterable, Tuple
+
 from rastervision.pipeline.config import register_config
 from rastervision.core.evaluation.classification_evaluator_config import (
     ClassificationEvaluatorConfig)
 from rastervision.core.evaluation.object_detection_evaluator import (
     ObjectDetectionEvaluator)
 
+if TYPE_CHECKING:
+    from rastervision.core.data import ClassConfig
+
 
 @register_config('object_detection_evaluator')
 class ObjectDetectionEvaluatorConfig(ClassificationEvaluatorConfig):
-    def build(self, class_config):
-        return ObjectDetectionEvaluator(class_config, self.output_uri)
+    def build(self, class_config: 'ClassConfig',
+              scene_group: Tuple[str, Iterable[str]]
+              ) -> ObjectDetectionEvaluator:
+        group_name, _ = scene_group
+        output_uri = self.get_output_uri(group_name)
+        return ObjectDetectionEvaluator(class_config, output_uri)
